d0d19159925444a283680f039fa2c17e7b2d6777
Pavel <you@example.com>
2023-11-17 23:15:06 +0300

    feat: implement task26 (17.11.23 23:14)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -499,10 +499,14 @@ function unbracketTag(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  const out = [];
+  str.split(';').forEach((e) => {
+    out.push(e);
+  });
+  return out;
 }
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 /**
  * Encode specified string with ROT13 cipher
  * See details:  https://en.wikipedia.org/wiki/ROT13
