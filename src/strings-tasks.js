0752b005fb7bca776ee558884e064bebd9f1ffa3
Pavel <you@example.com>
2023-11-17 21:02:02 +0300

    feat: implement task16 (17.11.23 21:01)

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -294,10 +294,10 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 /**
  * Checks if a given string contains a specified substring.
  *
